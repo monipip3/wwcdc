d69436dd03a58e1ecf478f981f9762f39829fb82
Monica P <16262065+monipip3@users.noreply.github.com>
2018-11-16 14:31:40 -0500

    added actors and directors

--- webscraping/imdb_answers.py
+++ webscraping/imdb_answers.py
@@ -12,38 +12,70 @@ import json
 
 r = requests.get('https://www.imdb.com/title/tt0071853/')
 
+#print r 
 r_unparsed = r.text
 
-### now make some Soup using Beautiful Soup and XML parser
+
+# ### now make some Soup using Beautiful Soup and XML parser
+#start = time.time()
 b = BeautifulSoup(r_unparsed,'lxml')
+#end = time.time()
+#print end - start
 
+print b
 
-### extract the title and save it into a variable
+# ### extract the title and save it into a variable
+##### you can use some specific methods of Beautiful Soup or follow the tree
+#### print b.find('h1').text
 title = b.title.text
 print(title)
 
-### if there was more than 1 title it would have to be
+# ### if there was more than 1 title it would have to be
 
 title = b.find_all('title')
 print(title)
 
-### extract the description and save it into a variable
+# ### extract the description and save it into a variable
+# ### extract the Rating eg: R and save into a variable
 
 desc = b.find('div','summary_text').text.strip()
-print(desc)
+# print(desc)
 
-### extract the Rating eg: R and save into a variable
+#print b.find('div','subtext').text.strip()[0:2]
 
-# rating = b.find('script',type='application/ld+json').text.strip()
-# print(rating)	
-
-
+#rating = b.find('script',type='application/ld+json').text.strip()
+#print(rating)	
 
 rating = json.loads(b.find('script', type='application/ld+json').text)['contentRating']
 
 
-## create a function that extracts this information of any IMDB movie of your choosing
-## ^ into the form of a dictionary 
+## extract the actors 
+actors = json.loads(b.find('script', type='application/ld+json').text)['actor']
+
+actors_list = []
+
+for actor in actors:
+	actors_list.append(actor[u'name'])
+
+def actors(x):
+	actors_list = []
+	actors = json.loads(x.find('script', type='application/ld+json').text)['actor']
+	for actor in actors:
+		actors_list.append(str(actor['name']))
+	return actors_list
+
+def directors(x):
+	directors = json.loads(x.find('script', type='application/ld+json').text)['director']
+	directors_list = []
+	for director in directors:
+		directors_list.append(str(director['name']))
+	return directors_list
+
+print directors(b)
+
+
+# ## create a function that extracts this information of any IMDB movie of your choosing
+# ## ^ into the form of a dictionary 
 
 def movie_info(id):
 	r = requests.get('https://www.imdb.com/title/{0}/'.format(id))
@@ -53,6 +85,8 @@ def movie_info(id):
 	movie_dict[id]['title'] = b.title.text
 	movie_dict[id]['desc'] = b.find('div','summary_text').text.strip()
 	movie_dict[id]['rating'] = json.loads(b.find('script', type='application/ld+json').text)['contentRating']
+	movie_dict[id]['actors'] = actors(b)
+	# movie_dict[id]['directors'] = directors(b)
 	return movie_dict
 
 Adrift = movie_info('tt6306064')
